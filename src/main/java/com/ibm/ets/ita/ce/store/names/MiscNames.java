7bdfec8ebd3e3dfb1b078bf3b7dad053d483dca7
Dave Braines <davebraines@Daves-MacBook-Pro.local>
2019-07-02 10:25:50 -0400

    Incremented version number

--- src/main/java/com/ibm/ets/ita/ce/store/names/MiscNames.java
+++ src/main/java/com/ibm/ets/ita/ce/store/names/MiscNames.java
@@ -9,7 +9,7 @@ public class MiscNames {
 	public static final String copyrightNotice = "(C) Copyright IBM Corporation  2011, 2017";
 
 	//Main
-	public static final String VERSION = "1.3.0003";
+	public static final String VERSION = "1.3.0004";
 	public static final String CESTORENAME_DEFAULT = "DEFAULT";
 	public static final String CM_GLOBAL = "global";
 
